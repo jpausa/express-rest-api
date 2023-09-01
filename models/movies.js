d165880cadc95d4c3164736b3920032590946c60
Jorge <glph8911@gmail.com>
2023-09-01 14:47:08 -0500

    refactor using classes to isolate model logic

---
+++ models/movies.js
@@ -0,0 +1,46 @@
+import { readJSON } from "../utils/custom-require.js"
+import { randomUUID } from "node:crypto"
+
+const movies = readJSON("../movies.json")
+
+export class MovieModel {
+  static async getAll({ genre }) {
+    if (genre) {
+      return movies.filter((movie) =>
+        movie.genre.some(
+          (movieGenre) => movieGenre.toLowerCase() === genre.toLowerCase()
+        )
+      )
+    }
+    return movies
+  }
+
+  static async getById({ id }) {
+    return movies.find((movie) => movie.id === id)
+  }
+
+  static async create({ data }) {
+    const newMovie = {
+      id: randomUUID(),
+      ...data,
+    }
+    movies.push(newMovie)
+    return newMovie
+  }
+
+  static async update({ id, data }) {
+    const movieIndex = movies.findIndex((movie) => movie.id === id)
+
+    if (movieIndex < 0) {
+      return false
+    }
+    console.log(data)
+    const updatedMovie = {
+      ...movies[movieIndex],
+      ...data,
+    }
+    console.log(updatedMovie)
+    movies[movieIndex] = updatedMovie
+    return updatedMovie
+  }
+}
--- router/movies.js
+++ router/movies.js
@@ -1,73 +1,47 @@
 import { Router } from "express"
-import { readJSON } from "../utils/custom-require.js"
-import { randomUUID } from "node:crypto"
 import { validateMovie, validatePartialMovie } from "../schemas/movies.js"
+import { MovieModel } from "../models/movies.js"
 
 export const movieRouter = Router()
 
-const movies = readJSON("../movies.json")
-
-movieRouter.get("/", (req, res) => {
+movieRouter.get("/", async (req, res) => {
   const { genre } = req.query
 
-  if (genre) {
-    const filteredMovies = movies.filter((movie) =>
-      movie.genre.some(
-        (movieGenre) => movieGenre.toLowerCase() === genre.toLowerCase()
-      )
-    )
-    return filteredMovies.length > 0
-      ? res.json(filteredMovies)
-      : res.status(404).json({ error: "Genre not found" })
-  }
-  res.json(movies)
+  const movies = await MovieModel.getAll({ genre })
+  return res.json(movies)
 })
 
-movieRouter.get("/:id", (req, res) => {
+movieRouter.get("/:id", async (req, res) => {
   const { id } = req.params
-  const movie = movies.find((movie) => movie.id === id)
-  if (movie) {
-    res.json(movie)
-  } else {
-    res.status(404)
-    res.json({ error: "Movie not found" })
-  }
+  const movie = await MovieModel.getById({ id })
+
+  if (movie) return res.json(movie)
+  return res.status(404).json({ error: "Movie not found" })
 })
 
-movieRouter.post("/", (req, res) => {
+movieRouter.post("/", async (req, res) => {
+  const body = req.body
+  const validationResult = validateMovie(body)
+  if (validationResult.error) {
+    return res.status(400).json(JSON.parse(validationResult.error.message))
+  }
+
+  const newMovie = await MovieModel.create({ data: body })
+  return res.status(201).json(newMovie)
+})
+
+movieRouter.patch("/:id", async (req, res) => {
   const body = req.body
 
-  const validationResult = validateMovie(body)
-
+  const validationResult = validatePartialMovie(body)
   if (validationResult.error) {
     return res.status(400).json(JSON.parse(validationResult.error.message))
   }
 
-  const newMovie = {
-    id: randomUUID(),
-    ...validationResult.data,
-  }
-  movies.push(newMovie)
-  res.status(201).json(newMovie)
-})
-
-movieRouter.patch("/:id", (req, res) => {
-  const validationResult = validatePartialMovie(req.body)
-
-  if (validationResult.error) {
-    return res.status(400).json(JSON.parse(validationResult.error.message))
-  }
   const { id } = req.params
-  const movieIndex = movies.movies.findIndex((movie) => movie.id === id)
 
-  if (movieIndex < 0) {
-    return res.status(404).json({ error: "Movie not found" })
-  }
+  const updatedMovie = await MovieModel.update({ id, data: body })
 
-  const updatedMovie = {
-    ...movies[movieIndex],
-    ...validationResult.data,
-  }
-  movies[movieIndex] = updatedMovie
-  res.json(updatedMovie)
+  if (updatedMovie) return res.json(updatedMovie)
+  return res.status(404).json({ error: "Movie not found" })
 })
