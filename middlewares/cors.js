95ec1a17de8d0641cbf36ae234a67bb7e9a66cbd
Jorge <glph8911@gmail.com>
2023-09-01 09:38:53 -0500

    refactor cors util function to accept origins from parameters

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,13 +1,14 @@
 import cors from "cors"
 
-export const corsMiddleware = () =>
+const ACCEPTED_ORIGINS = [
+  "http://localhost:3000",
+  "https://movies-app-react.netlify.app",
+]
+
+export const corsMiddleware = (acceptedOrigins = ACCEPTED_ORIGINS) =>
   cors({
     origin: (origin, callback) => {
-      const ACCEPTED_ORIGINS = [
-        "http://localhost:3000",
-        "https://movies-app-react.netlify.app",
-      ]
-      if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+      if (acceptedOrigins.includes(origin) || !origin) {
         return callback(null, true)
       }
       return new Error("Not allowed by CORS")
