c07b93d338e0e43959f830c3cd831e771d5c9b32
Jorge <glph8911@gmail.com>
2023-09-15 12:36:33 -0500

    installing standard js

---
+++ controllers/movies.js
@@ -0,0 +1,51 @@
+import { MovieModel } from "../models/movies"
+import { validateMovie, validatePartialMovie } from "../schemas/movies.js"
+import { MovieModel } from "../models/movies.js"
+
+export class MovieController {
+  constructor(movieService) {
+    this.movieService = movieService
+  }
+
+  static async getAllMovies(req, res) {
+    const { genre } = req.query
+
+    const movies = await MovieModel.getAll({ genre })
+    return res.json(movies)
+  }
+
+  static async getMovieById(req, res) {
+    const { id } = req.params
+    const movie = await MovieModel.getById({ id })
+
+    if (movie) return res.json(movie)
+    return res.status(404).json({ error: "Movie not found" })
+  }
+
+  static async createMovie(req, res) {
+    const body = req.body
+    const validationResult = validateMovie(body)
+    if (validationResult.error) {
+      return res.status(400).json(JSON.parse(validationResult.error.message))
+    }
+
+    const newMovie = await MovieModel.create({ data: body })
+    return res.status(201).json(newMovie)
+  }
+
+  static async updateMovie(req, res) {
+    const body = req.body
+
+    const validationResult = validatePartialMovie(body)
+    if (validationResult.error) {
+      return res.status(400).json(JSON.parse(validationResult.error.message))
+    }
+
+    const { id } = req.params
+
+    const updatedMovie = await MovieModel.update({ id, data: body })
+
+    if (updatedMovie) return res.json(updatedMovie)
+    return res.status(404).json({ error: "Movie not found" })
+  }
+}
--- router/movies.js
+++ router/movies.js
@@ -1,47 +1,12 @@
-import { Router } from "express"
-import { validateMovie, validatePartialMovie } from "../schemas/movies.js"
-import { MovieModel } from "../models/movies.js"
+import { Router } from 'express'
+import { MovieController } from '../controllers/movies.js'
 
 export const movieRouter = Router()
 
-movieRouter.get("/", async (req, res) => {
-  const { genre } = req.query
+movieRouter.get('/', MovieController.getAllMovies)
 
-  const movies = await MovieModel.getAll({ genre })
-  return res.json(movies)
-})
+movieRouter.get('/:id', MovieController.getMovieById)
 
-movieRouter.get("/:id", async (req, res) => {
-  const { id } = req.params
-  const movie = await MovieModel.getById({ id })
+movieRouter.post('/', MovieController.createMovie)
 
-  if (movie) return res.json(movie)
-  return res.status(404).json({ error: "Movie not found" })
-})
-
-movieRouter.post("/", async (req, res) => {
-  const body = req.body
-  const validationResult = validateMovie(body)
-  if (validationResult.error) {
-    return res.status(400).json(JSON.parse(validationResult.error.message))
-  }
-
-  const newMovie = await MovieModel.create({ data: body })
-  return res.status(201).json(newMovie)
-})
-
-movieRouter.patch("/:id", async (req, res) => {
-  const body = req.body
-
-  const validationResult = validatePartialMovie(body)
-  if (validationResult.error) {
-    return res.status(400).json(JSON.parse(validationResult.error.message))
-  }
-
-  const { id } = req.params
-
-  const updatedMovie = await MovieModel.update({ id, data: body })
-
-  if (updatedMovie) return res.json(updatedMovie)
-  return res.status(404).json({ error: "Movie not found" })
-})
+movieRouter.patch('/:id', MovieController.updateMovie)
