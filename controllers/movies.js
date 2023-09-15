f3946b43fb1ab610ba4e64a536846b1231dcf9af
Jorge <glph8911@gmail.com>
2023-09-15 15:36:29 -0500

    creating dir for local movies files and fixing lint issues

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,4 +1,4 @@
-import { MovieModel } from '../models/movies.js'
+import { MovieModel } from '../models/local-file-system/movies.js'
 import { validateMovie, validatePartialMovie } from '../schemas/movies.js'
 
 export class MovieController {
--- models/local-file-system/movies.js
+++ models/local-file-system/movies.js
@@ -1,4 +1,4 @@
-import { readJSON } from '../utils/custom-require.js'
+import { readJSON } from '../../utils/custom-require.js'
 import { randomUUID } from 'node:crypto'
 
 const movies = readJSON('../movies.json')
